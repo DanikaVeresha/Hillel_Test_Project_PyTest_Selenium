4f354ea6c3c7b9a6e745df3d67a44f26082e702c
DanikaVeresha <163162916+DanikaVeresha@users.noreply.github.com>
2024-07-11 18:18:17 +0300

    Delete Web_tests/Helpers/error_message.py

--- Web_tests/Helpers/error_message.py
+++
@@ -1,8 +0,0 @@
-"""
-This file contains the class ErrorMessage, which is used to store error messages.
-"""
-
-
-class ErrorMessage:
-    def __init__(self, message):
-        self.error_message = message
